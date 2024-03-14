38cf3e330e7fd51e391b01d44721e2c29c79cfee
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-14 22:49:12 +0530

    comment-fix
    comment fix

--- todo-frontend/src/components/security/AuthContext.js
+++ todo-frontend/src/components/security/AuthContext.js
@@ -1,22 +1,19 @@
-//create a context
+
 
 
 import { createContext, useState } from "react";
 
+//1. create a context
 export const AuthContext = createContext()
-
-
 
 
 //2. share the created context with other components
 
 //auth provider will provide context to other components
-
 function AuthProvider({children}){
 
-    //put some values state in the context
+    //3. put some values state in the context
     const [number, setNumber] = useState(0)
-
 
     return (
         <AuthContext.Provider value={{number}}>

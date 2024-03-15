dbdceecaf1e2e93a5e4c4be7aba3e8a95d756c32
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-15 13:54:00 +0530

    axios-setup
    axios setup and calling spring rest api in frontend

--- todo-frontend/src/components/WelcomeComponent.jsx
+++ todo-frontend/src/components/WelcomeComponent.jsx
@@ -1,4 +1,5 @@
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 function WelcomeComponent() {
@@ -7,13 +8,37 @@ function WelcomeComponent() {
     const { username } = useParams()
     console.log(username)
 
+    function callHelloWorldRESTAPI(){
+        console.log(`called`)
+        axios.get("http://localhost:8080/hello-world")
+        .then( 
+            (response) =>   successResponse(response)
+        )
+        .catch(
+            (error) => errorResponse(error)
+        )
+        .finally(
+            () => console.log('cleanup')
+        )
+
+    }
+
+    function successResponse(response){
+        console.log(response);
+    }
+
+    function errorResponse(error){
+        console.log(error);
+    }
+
     return (
         <div className="WelcomeComponent">
             <h1> Welcome {username}</h1>
             {/* <div> Your todos <a href="/todos"> Go Here</a></div> */}
             <div> Your todos <Link to="/todos"> Go Here</Link></div>
-
+            <div className="btn btn-success m-5" onClick={callHelloWorldRESTAPI}> Call Hello World </div>
         </div>
+        
     )
 }
 

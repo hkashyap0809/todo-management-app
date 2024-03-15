2efc9ca7ffa957c8fe6d4382e781aa99cdf0aaee
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-15 14:08:18 +0530

    enabling-cors
    enabling cors in hello world bean rest api

--- todo-backend/src/main/java/com/todobackend/todobackend/TodoBackendApplication.java
+++ todo-backend/src/main/java/com/todobackend/todobackend/TodoBackendApplication.java
@@ -2,12 +2,27 @@ package com.todobackend.todobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoBackendApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+				.addMapping("/**")							//for mapping
+				.allowedMethods("*")						//for methods GET,PUT,POST
+				.allowedOrigins("http://localhost:3000");	//for origin , from where we are calling
+			}
+		};	
 	}
 
 }

3a16b614c9166df5a9f8c67ee42281d3f776bae2
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-20 19:49:36 +0530

    disabling-csrf
    adding spring security and basic authentication

---
+++ todo-backend/src/main/java/com/todobackend/todobackend/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.todobackend.todobackend.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+	
+	//we will configure spring security filter chain
+	
+	
+	//disabling csrf
+	//stateless rest api
+	
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+		auth -> {
+			auth.anyRequest().authenticated();
+		});
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		//stateless rst api
+		http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		
+		//disabling csrf
+		
+		http.csrf().disable();
+		return http.build();	
+	}
+
+}
